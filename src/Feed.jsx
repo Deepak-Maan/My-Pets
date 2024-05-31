2d6a9c980a9f397bc357cd2887982bc6f804f9e2
Deepak-Maan <jattdevta1008@gmail.com>
2024-05-31 11:35:50 +0530

    first commit

---
+++ src/Feed.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Feed = () => {
+    return (
+        <>
+            <div className="bg-slate-400">
+                <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia veniam optio cumque ab quod reprehenderit nisi, eum totam inventore consectetur nam nobis explicabo, ea perspiciatis dolores dolorum culpa laboriosam ut.</h1>
+            </div>
+        </>
+    )
+}
+
+export default Feed
