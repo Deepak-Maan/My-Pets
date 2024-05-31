8ab0de9c4415cfb98a62594332b8d0c710f5352b
Deepak-Maan <jattdevta1008@gmail.com>
2024-05-31 16:23:22 +0530

    code merged

--- src/components/Common/Helper.jsx
+++ src/components/Common/Helper.jsx
@@ -3,13 +3,13 @@ import ExploreIcon from "../../assets/svg/footer/exploreIcon.svg"
 import SwapIcon from "../../assets/svg/footer/swapIcon.svg"
 import PetsIcon from "../../assets/svg/footer/petsIcon.svg"
 import HeartIcon from "../../assets/svg/footer/heartIcon.svg"
-import FishGreen from "../../assets/images/svg/FishGreen.svg";
-import RedFish from "../../assets/images/svg/badge.svg";
-import Mashroom from "../../assets/images/svg/Mashrrom.svg";
-import Coin from "../../assets/images/svg/Coin.svg";
-import Shield from "../../assets/images/svg/shield.svg";
-import Bonk from "../../assets/images/svg/bonk.svg";
-import Time from "../../assets/images/svg/time.svg";
+import FishGreen from "../../assets/svg/FishGreen.svg";
+import RedFish from "../../assets/svg/badge.svg";
+import Mashroom from "../../assets/svg/Mashrrom.svg";
+import Coin from "../../assets/svg/Coin.svg";
+import Shield from "../../assets/svg/shield.svg";
+import Bonk from "../../assets/svg/bonk.svg";
+import Time from "../../assets/svg/time.svg";
 
 // navData
 export const Colors = [
