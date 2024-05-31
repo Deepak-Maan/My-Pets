0e76f877a1a550c5268bd2b22d4d6ad028826a15
naman-398 <you@example.com>
2024-05-31 13:52:10 +0530

    button done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import All from "./components/All.jsx";
 import Dropdown from "./components/Dropdown.jsx";
 function App() {
   return (
     <div className="">
-      <Dropdown />
+      {/* <Dropdown /> */}
+      <All/>
     </div>
   );
 }
