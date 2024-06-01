85fc3c4b9cf94042d774aad6d38c390cf566982e
Deepak-Maan <jattdevta1008@gmail.com>
2024-06-01 11:55:24 +0530

    modified css

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import Navbar from "./components/Common/Navbar.jsx";
 import Footer from "./components/Common/Footer.jsx";
 function App() {
   return (
-    <div>
-      <div className="bg-[#DCFCE7] min-h-screen max-w-[394px] mx-auto ">
+    <div className=" min-h-screen">
+      <div className="bg-[#DCFCE7] max-w-[394px] mx-auto ">
         <Navbar />
         <All />
       </div>
       <div className="bg-[#F0FDF4] max-w-[394px] mx-auto">
-        <Footer />
+        <Footer className="fixed" />
       </div>
     </div>
   );
